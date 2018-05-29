e13e6f08e56825b153ffca377897fd16a301affb
Hisato Kawaminami and Nick Rogers <nick@>
2018-05-29 16:09:05 -0700

    Add border and highlight to paragraphs upon clicking them.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $(".intro").click(function() {
+    $("div").removeClass("intro-box");
+    $(".intro").addClass("intro-box");
+  });
+  $(".paragraph1").click(function() {
+    $("div").removeClass("intro-box");
+    $(".paragraph1").addClass("intro-box");
+  });
+  $(".paragraph2").click(function() {
+    $("div").removeClass("intro-box");
+    $(".paragraph2").addClass("intro-box");
+  });
+  $(".paragraph3").click(function() {
+    $("div").removeClass("intro-box");
+    $(".paragraph3").addClass("intro-box");
+  });
+  $(".closing").click(function() {
+    $("div").removeClass("intro-box");
+    $(".closing").addClass("intro-box");
+  });
+});
